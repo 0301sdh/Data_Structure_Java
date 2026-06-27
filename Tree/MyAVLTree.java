90a22eec836beb509c84621bf06af69464d48750
DongHwan Seo <0301sdh@naver.com>
2026-06-27 18:02:26 +0900

    MYAVLTree version2 not done yet

--- Tree/MyAVLTree.java
+++ Tree/MyAVLTree.java
@@ -3,77 +3,198 @@ package Tree;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MyAVLTree{
+public class MyAVLTree<T extends Comparable<T>> {
 
-     private static class Node{
-        int key;
-        int height;
-        Node left, right;
+   private static class Node<T> {
+      T data;
+      int height;
+      Node<T> left, right;
 
-        Node(int key){
-            this.key = key;
-            this.height = 1;
-        }
-     }
+      Node(T data) {
+         this.data = data;
+         this.height = 1;
+      }
+   }
 
-     private Node root;
+   private Node<T> root;
 
-     private int height(Node node){
-        return (node == null) ? 0 : node.height;
-     }
+   private int height(Node<T> node) {
+      return (node == null) ? 0 : node.height;
+   }
 
-     private int balanceFactor(Node node){
-        return (node == null) ? 0 : height(node.left) - height(node.right);
-     }
+   private int balanceFactor(Node<T> node) {
+      return (node == null) ? 0 : height(node.left) - height(node.right);
+   }
 
-     private void updateHeight(Node node){
-        node.height = 1 + Math.max(height(node.left), height(node.right));
-     }
+   private void updateHeight(Node<T> node) {
+      node.height = 1 + Math.max(height(node.left), height(node.right));
+   }
 
-    // ===== 회전 =====
+   // ===== 회전 =====
 
-    // 오른쪽 회전 (LL 케이스)
-    //        y                x
-    //       / \              / \
-    //      x   T3   --->    T1  y
-    //     / \                  / \
-    //    T1  T2               T2  T3
+   // 오른쪽 회전 (LL 케이스)
+   // y x
+   // / \ / \
+   // x T3 ---> T1 y
+   // / \ / \
+   // T1 T2 T2 T3
 
-     private Node rotateRight(Node y){
-        Node x = y.left;
-        Node T2 = x.right;
+   private Node<T> rotateRight(Node<T> y) {
+      Node<T> x = y.left;
+      Node<T> T2 = x.right;
 
-        x.right = y;
-        y.left = T2;
-        updateHeight(y);
-        updateHeight(x);
-        return x;
-     }
+      x.right = y;
+      y.left = T2;
+      updateHeight(y);
+      updateHeight(x);
+      return x;
+   }
 
-    // 왼쪽 회전 (RR 케이스)
-    //      x                    y
-    //     / \                  / \
-    //    T1  y      --->      x   T3
-    //       / \              / \
-    //      T2  T3           T1  T2
+   // 왼쪽 회전 (RR 케이스)
+   // x y
+   // / \ / \
+   // T1 y ---> x T3
+   // / \ / \
+   // T2 T3 T1 T2
 
-    private Node rotateLeft(Node x){
-        Node y = x.right;
-        Node T2 = y.left;
+   private Node<T> rotateLeft(Node<T> x) {
+      Node<T> y = x.right;
+      Node<T> T2 = y.left;
 
-        y.left = x;
-        x.right = T2;
+      y.left = x;
+      x.right = T2;
 
-        updateHeight(x);
-        updateHeight(y);
-        return y;
-    }
+      updateHeight(x);
+      updateHeight(y);
+      return y;
+   }
 
-    private Node rebalance(Node node){
-        updateHeight(node);
-        int balance = balanceFactor(node);
-        
-    }
+   private Node<T> rebalance(Node<T> node) {
+      updateHeight(node);
+      int balance = balanceFactor(node);
 
+      // LL : 왼쪽이 무겁고 왼쪽 자식이 왼쪽으로 치우침
+      if (balance > 1 && balanceFactor(node.left) >= 0)
+         return rotateRight(node);
+
+      // LR : 왼쪽이 무겁고 왼쪽 자식이 오른쪽으로 치우침
+      if (balance > 1 && balanceFactor(node.left) < 0) {
+         node.left = rotateLeft(node.left);
+         return rotateRight(node);
+      }
+
+      // RR : 오른쪽이 무겁고 오른쪽 자식이 오른쪽으로 치우침
+      if (balance < -1 && balanceFactor(node.right) <= 0)
+         return rotateLeft(node);
+
+      // RL : 오른쪽이 무겁고 오른쪽 자식이 왼쪽으로 치우침
+      if (balance < -1 && balanceFactor(node.right) > 0) {
+         node.right = rotateRight(node.right);
+         return rotateLeft(node);
+      }
+
+      return node; // 이미 균형
+   }
+
+   // insert : 데이터 삽입
+   public void insert(T data) {
+      root = insert(root, data);
+   }
+
+   private Node<T> insert(Node<T> node, T data) {
+      if (node == null)
+         return new Node<>(data);
+
+      int cmp = data.compareTo(node.data);
+      if (cmp < 0) {
+         node.left = insert(node.left, data);
+      } else if (cmp > 0) {
+         node.right = insert(node.right, data);
+      } else { // 중복일때
+         return node;
+      }
+      return rebalance(node);
+   }
+
+   // 검색
+   public boolean search(T data) {
+      return search(root, data);
+   }
+
+   private boolean search(Node<T> node, T data) {
+      if (node == null)
+         return false;
+      int cmp = data.compareTo(node.data);
+      if (cmp < 0) {
+         return search(node.left, data);
+      } else if (cmp > 0) {
+         return search(node.right, data);
+      } else
+         return true;
+   }
+
+   // 삭제
+
+   public void delete(T data) {
+      root = delete(root, data);
+   }
+
+   private Node<T> delete(Node<T> node, T data) {
+      if (node == null)
+         return null;
+      int cmp = data.compareTo(node.data);
+      if (cmp < 0) {
+         node.left = delete(node.left, data);
+      } else if (cmp > 0) {
+         node.right = delete(node.right, data);
+      } else {
+         if (node.left == null)
+            return node.right;
+         if (node.right == null)
+            return node.left;
+         Node<T> minNode = findMin(node.right);
+         node.data = minNode.data;
+         node.right = delete(node.right, minNode.data);
+      }
+      return rebalance(node);
+   }
+
+   private Node<T> findMin(Node<T> node) {
+      while (node.left != null) {
+         node = node.left;
+      }
+      return node;
+   }
+
+   public boolean contains(T data) {
+      Node<T> current = root;
+      if (current == null) {
+         return false;
+      }
+      while (current != null) {
+         int cmp = data.compareTo(current.data);
+         if (cmp < 0) {
+            current = current.left;
+         } else if (cmp > 0) {
+            current = current.right;
+         } else {
+            return true;
+         }
+      }
+      return false;
+   }
+
+   public void inorder() {
+      inorder(root);
+      System.out.println();
+   }
+
+   private void inorder(Node<T> node) {
+      if (node == null)
+         return;
+      inorder(node.left);
+      System.out.print(node.data + " ");
+      inorder(node.right);
+   }
 
 }
