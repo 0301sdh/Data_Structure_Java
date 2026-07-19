becb5f91b35dc757a02d632a56a039963646fa51
DongHwan Seo <0301sdh@naver.com>
2026-07-19 19:31:29 +0900

    make Heap with java

---
+++ Heap/MaxHeap.java
@@ -0,0 +1,142 @@
+package Heap;
+
+public class MaxHeap<T extends Comparable<T>> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object[] heap;
+    private int size;
+    private int capacity;
+
+    public MaxHeap() {
+        this.capacity = DEFAULT_CAPACITY;
+        this.size = 0;
+        this.heap = new Object[capacity];
+    }
+
+    private int parent(int i) {
+        return (i - 1) / 2;
+    }
+
+    private int leftChild(int i) {
+        return i * 2 + 1;
+    }
+
+    private int rightChild(int i) {
+        return 2 * i + 2;
+    }
+
+    private void swap(int i, int j) {
+        Object tmp = heap[i];
+        heap[i] = heap[j];
+        heap[j] = tmp;
+    }
+
+    @SuppressWarnings("unchecked")
+    public T peek() {
+        if (size == 0) {
+            throw new IllegalStateException("Heap is empty");
+        }
+        return (T) heap[0];
+    }
+
+    public void insert(T value) {
+        if (size == capacity) {
+            resize();
+        }
+        heap[size] = value;
+        size++;
+        siftUp(size - 1);
+    }
+
+    @SuppressWarnings("unchecked")
+    private void siftUp(int i) {
+        while (i > 0 && ((T) heap[i]).compareTo((T) heap[parent(i)]) > 0) {
+            swap(i, parent(i));
+            i = parent(i);
+        }
+    }
+
+    private void resize() {
+        capacity = capacity * 2;
+        Object[] newHeap = new Object[capacity];
+        for (int i = 0; i < size; i++) {
+            newHeap[i] = heap[i];
+        }
+        heap = newHeap;
+    }
+
+    @SuppressWarnings("unchecked")
+    public T poll() {
+        if (size == 0) {
+            throw new IllegalStateException("Heap is empty");
+        }
+        T max = (T) heap[0];
+        heap[0] = heap[size - 1];
+        heap[size - 1] = null;
+        size--;
+        siftDown(0);
+        return max;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void siftDown(int i) {
+        while (true) {
+            int largest = i;
+            int left = leftChild(i);
+            int right = rightChild(i);
+
+            if (left < size && ((T) heap[left]).compareTo((T) heap[largest]) > 0) {
+                largest = left;
+            }
+            if (right < size && ((T) heap[right]).compareTo((T) heap[largest]) > 0) {
+                largest = right;
+            }
+            if (largest == i) {
+                break;
+            }
+
+            swap(i, largest);
+            i = largest;
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public static void main(String[] args) {
+        MaxHeap<Integer> heap = new MaxHeap<>();
+
+        heap.insert(5);
+        heap.insert(3);
+        heap.insert(8);
+        heap.insert(1);
+        heap.insert(9);
+        heap.insert(2);
+        heap.insert(20);
+        heap.insert(21);
+        heap.insert(25);
+        heap.insert(62);
+        heap.insert(24);
+        heap.insert(25);
+        heap.insert(200);
+        heap.insert(72);
+
+        System.out.print("정렬 결과 : ");
+        while (!heap.isEmpty()) {
+            System.out.print(heap.poll() + " ");
+        }
+        System.out.println();
+
+        MaxHeap<String> words = new MaxHeap<>();
+        words.insert("apple");
+        words.insert("cherry");
+        words.insert("banana");
+        System.out.println("last word : " + words.peek());
+
+    }
+
+}
